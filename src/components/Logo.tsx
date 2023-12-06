889d368e077d09412c4fdee38c148d79a399e069
SamerBenMim <samer.benmim@insat.ucar.tn>
2023-12-06 21:59:21 +0100

    5

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,8 +7,7 @@ export default function Logo({ sx }: BoxProps) {
     <RouterLink to={`/${MAIN_PATH.browse}`}>
       <Box
         component="img"
-        alt="Netflix Logo"
-        src="/assets/netflix-logo.png"
+        alt="MOVIEFLIX"
         width={87}
         height={25}
         sx={{
